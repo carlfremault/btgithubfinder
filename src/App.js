1588a3c59d61676cf2f2fada915296c4b440ccb1
Carl Fremault <carlfremault@yahoo.com>
2022-08-19 14:19:35 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
-import axios from 'axios';
+import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import { Home } from './components/pages/Home';
+import { NotFound } from './components/pages/NotFound';
+import User from './components/users/User';
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
 
-const github = axios.create({
-  baseURL: 'https://api.github.com',
-  headers: { Authorization: process.env.REACT_APP_GITHUB_TOKEN },
-});
-
-class App extends Component {
-  state = {
-    users: [],
-    loading: false,
-  };
-
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-  //   const res = await github.get('/users');
-  //   this.setState({ users: res.data, loading: false });
-  // }
-
-  // Search Github users
-  searchUsers = async (text) => {
-    this.setState({ loading: true });
-    const res = await github.get(`/search/users?q=${text}`);
-    this.setState({ users: res.data.items, loading: false });
-  };
-
-  render() {
-    return (
-      <div className='App'>
-        <Navbar title='Github Finder' icon='fab fa-github' />
-        <div className='container'>
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar title='Github Finder' icon='fab fa-github' />
+            <div className='container'>
+              <Alert />
+              <Routes>
+                <Route exact path='/' element={<Home />} />
+                <Route exact path='/about' element={<About />} />
+                <Route exact path='/user/:userName' element={<User />} />
+                <Route path='*' element={<NotFound />} />
+              </Routes>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
+  );
+};
 
 export default App;
---
+++ src/context/github/GithubState.jsx
@@ -0,0 +1,77 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './GithubContext';
+import GithubReducer from './GithubReducer';
+import {
+  SEARCH_USERS,
+  SET_LOADING,
+  CLEAR_USERS,
+  GET_USER,
+  GET_REPOS,
+} from '../types';
+
+const github = axios.create({
+  baseURL: 'https://api.github.com',
+  headers: { Authorization: process.env.REACT_APP_GITHUB_TOKEN },
+});
+
+const GithubState = (props) => {
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+  // Search Users
+  const searchUsers = async (text) => {
+    setLoading();
+    const res = await github.get(`/search/users?q=${text}`);
+    dispatch({ type: SEARCH_USERS, payload: res.data.items });
+  };
+
+  // Get User
+  const getUser = async (username) => {
+    setLoading();
+    const res = await github.get(`/users/${username}?`);
+    dispatch({ type: GET_USER, payload: res.data });
+  };
+
+  // Get Repos
+  const getUserRepos = async (username) => {
+    setLoading();
+    const res = await github.get(
+      `/users/${username}/repos?per_page=5&sort=created:asc?`
+    );
+    dispatch({ type: GET_REPOS, payload: res.data });
+    // setRepos(res.data);
+    // setLoading(false);
+  };
+
+  // Clear Users
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
+
+  // Set Loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        user: state.user,
+        repos: state.repos,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+        getUser,
+        getUserRepos,
+      }}
+    >
+      {props.children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubState;
